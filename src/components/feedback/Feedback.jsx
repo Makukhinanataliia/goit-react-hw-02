e4ece2cc35e1739ae15f1e9b5197451b0acff056
Makukhinanataliia <makukhinanataliia@gmail.com>
2024-04-01 00:00:54 -0400

    fix totalFeedback

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -6,15 +6,13 @@ const Feedback = ({
   positiveReviewPercentage,
 }) => {
   return (
-    totalFeedback > 0 && (
-      <ul className={css.feedbackList}>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>Total: {totalFeedback}</li>
-        <li>Positive: {positiveReviewPercentage}%</li>
-      </ul>
-    )
+    <ul className={css.feedbackList}>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>Total: {totalFeedback}</li>
+      <li>Positive: {positiveReviewPercentage}%</li>
+    </ul>
   );
 };
 export default Feedback;
